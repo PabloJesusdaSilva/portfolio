7c8954f81be637415d01796c097e560aa4653a71
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-10-17 11:18:48 -0200

    :fix: fixing padding error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Header () {
    return (
       <header className="fixed z-20 flex justify-between w-full py-5 px-8 text-zinc-200 backdrop-blur-xl">
-         <div className="font-bold text-2xl transition-all duration-300 hover:text-zinc-300">
+         <div className="p-2 font-bold text-2xl transition-all duration-300 cursor-pointer hover:text-zinc-300">
             <Link href="/">
                P
             </Link>
@@ -14,12 +14,12 @@ export default function Header () {
 
          <nav className="hidden md:block">
             <ul className="flex space-x-6">
-               <li>
+               <li className="p-2 cursor-pointer">
                   <Link href="/about-me"className="text-xl font-semibold transition-all duration-300 hover:text-zinc-300">
                      About
                   </Link> 
                </li>
-               <li>
+               <li className="p-2 cursor-pointer">
                   <Link href="/projects" className="text-xl font-semibold transition-all duration-300 hover:text-zinc-300">
                      Projects
                   </Link> 
@@ -27,7 +27,7 @@ export default function Header () {
             </ul>
          </nav>
 
-         <div>
+         <div className="p-2 cursor-pointer">
             <Command className="transition-all duration-500 hover:rotate-180"
             />
          </div>
