4a24dc0e3c58ffdfca99b8f55f461f54cd11651b
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-11-29 15:59:34 -0300

    :feat: creating data

--- src/app/partials/ProjectSection.tsx
+++ src/app/partials/ProjectSection.tsx
@@ -4,28 +4,48 @@ import VtexImage from '@/app/assets/vtex-page.png'
 import AnunxImage from '@/app/assets/anunx-page.png'
 import MemojiImage from '@/app/assets/memoji.jpeg'
 
+const data = [
+   {
+      image: MemojiImage,
+      title: 'Memoji',
+      repositoriePath: 'https://github.com/PabloJesusdaSilva/memoji',
+      deployPath: 'https://codemoji-red.vercel.app/'
+   },
+   {
+      image: VtexImage,
+      title: 'Vtex',
+      repositoriePath: 'https://github.com/PabloJesusdaSilva/vtex-teste',
+      deployPath: 'https://codemoji-red.vercel.app/'
+   },
+   {
+      image: MemojiImage,
+      title: 'Memoji',
+      repositoriePath: 'https://github.com/PabloJesusdaSilva/memoji',
+      deployPath: 'https://codemoji-red.vercel.app/'
+   },
+   {
+      image: AnunxImage,
+      title: 'Anunx',
+      repositoriePath: 'https://github.com/PabloJesusdaSilva/Anunx',
+      deployPath: 'https://codemoji-red.vercel.app/'
+   },
+]
+
 const ProjectSection = () => {
    return(
       <section className='mt-16 ml-4'>
          <div className='flex flex-col items-center space-y-32'>
-            <ProjectCard 
-               image={MemojiImage}
-               title='Memoji'
-               repositoriePath='https://github.com/PabloJesusdaSilva/memoji'
-               deployPath='https://codemoji-red.vercel.app/'
-            />
-            <ProjectCard 
-               image={AnunxImage}
-               title='Anunx'
-               repositoriePath='https://github.com/PabloJesusdaSilva/Anunx'
-               deployPath=''
-            />
-            <ProjectCard 
-               image={VtexImage}
-               title='Vtex'
-               repositoriePath='https://github.com/PabloJesusdaSilva/vtex-teste'
-               deployPath=''
-            />
+            {
+               data.map(card => (
+                  <ProjectCard 
+                     image={card.image}
+                     title={card.title}
+                     repositoriePath={card.repositoriePath}
+                     deployPath={card.deployPath}
+                  />
+               ))
+            }
+            
          </div>
       </section>
    )
